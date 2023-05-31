8a000cac5211759e8a6e5cbf0f82767709ac7b2e
tqlinh99 <tqlinh170399@gmail.com>
2023-05-31 19:54:29 +0700

    macro updated

--- Bai Tap C++/Quan Ly Sinh Vien/main.cpp
+++ Bai Tap C++/Quan Ly Sinh Vien/main.cpp
@@ -13,10 +13,14 @@
 
 using namespace std;
 
-typedef enum{
-    NAM,
-    NU
-}TypeGender;
+#define INPUT_TYPE(text, var)       \
+cout << text;                       \
+cin  >> var;                        \
+
+#define PRINT_TITLE()   \
+cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n"; \
+cout << "| STT |  ID   |      TEN      | GIOI TINH | TUOI | DIEM TOAN | DIEM LY | DIEM HOA | DTB  |    HOC LUC      |\n"; \
+cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n"; \
 
 typedef enum{
     GIOI,
@@ -37,7 +41,7 @@ class SinhVien{
     private:
         int ID;
         string NAME;
-        TypeGender GENDER;
+        string GENDER;
         uint8_t AGE;
         float DIEM_TOAN;
         float DIEM_LY;
@@ -45,12 +49,13 @@ class SinhVien{
         float DIEM_TRUNG_BINH;
         TypeRankAcademic HOC_LUC;
     public:
-        SinhVien(string name, TypeGender gender, uint8_t age, float diemToan, float diemLy, float diemHoa);
+        SinhVien(string name, string gender, uint8_t age, float diemToan, float diemLy, float diemHoa);
+        SinhVien();
         int getID();
         void setName(string name);
         string getName();
-        void setGender(TypeGender gender);
-        TypeGender getGender();
+        void setGender(string gender);
+        string getGender();
         void setAge(uint8_t age);
         uint8_t getAge();
         void setDiemToan(float toan);
@@ -76,7 +81,7 @@ class SinhVien{
 * Output:
 *    None
 */
-SinhVien::SinhVien(string name, TypeGender gender, uint8_t age, float diemToan, float diemLy, float diemHoa){
+SinhVien::SinhVien(string name, string gender, uint8_t age, float diemToan, float diemLy, float diemHoa){
     static uint8_t id = 100;
     SinhVien::ID = id;
     id++;
@@ -86,6 +91,9 @@ SinhVien::SinhVien(string name, TypeGender gender, uint8_t age, float diemToan, 
     DIEM_TOAN = diemToan;
     DIEM_LY = diemLy;
     DIEM_HOA = diemHoa;
+}
+
+SinhVien::SinhVien(){   
 }
 
 /*
@@ -132,7 +140,7 @@ string SinhVien::getName(){
  * Output:
  *    none
 */
-void SinhVien::setGender(TypeGender gender){
+void SinhVien::setGender(string gender){
     GENDER = gender;
 }
 
@@ -144,7 +152,7 @@ void SinhVien::setGender(TypeGender gender){
  * Output:
  *    return SinhVien::GENDER in type of TypeGender
 */
-TypeGender SinhVien::getGender(){
+string SinhVien::getGender(){
     return this->GENDER;
 }
 
@@ -276,6 +284,16 @@ TypeRankAcademic SinhVien::getHocLuc(){
     }
 }
 
+typedef enum{
+            GIOI_TINH,
+            DIEM
+        }TypeDoiTuong;
+
+typedef enum{
+            NAME,
+            GPA
+        }TypeOfSort;
+
 /*
  * Function: Class Menu
  * Description: The class contains all properties and methods of a menu
@@ -289,13 +307,13 @@ class Menu{
         vector<SinhVien> Database;
     public:
         Menu();
+        void enterInfo(uint8_t mode, uint8_t index);
         void themSinhVien();
         void capNhatThongTin();
         void xoaSinhVien();
         void timSinhVien();
-        void sortByGPA();
-        void sortByName();
-        void hienThiDanhSach();
+        void sort(TypeOfSort sortType);
+        void hienThiDanhSach(SinhVien sv,uint8_t index);
 };
 
 /*
@@ -308,7 +326,11 @@ class Menu{
 */
 Menu::Menu(){
     int phim = 0;
-    
+    string name;
+
+    uint8_t index = 0;
+    uint8_t infoFound = 0;
+
     while(phim!=8){
         switch (phim)
         {
@@ -330,7 +352,7 @@ Menu::Menu(){
             break;
     
         case 1:
-            themSinhVien();
+            enterInfo(0,0);
             do
             {
                 cout << "\nMENU QUAN LY SINH VIEN(1)\n";
@@ -342,7 +364,21 @@ Menu::Menu(){
             break;
         
         case 2:
-            capNhatThongTin();
+            int id;
+            infoFound = 0;
+            index = 0;
+
+            INPUT_TYPE("\nNhap ID can cap nhat: ", id);
+
+            for(SinhVien sv : Database){
+                if(sv.getID() == id){
+                    infoFound = 1;
+                    enterInfo(1, index);
+                }
+                index++;
+            }
+            if (infoFound == 0)   cout << "\n    ID not found!\n";
+
             phim = 0;
             break;
 
@@ -352,24 +388,40 @@ Menu::Menu(){
             break;
 
         case 4:
-            timSinhVien();
+            index = 1;
+
+            INPUT_TYPE("\nNhap ten sinh vien can tim: ", name);
+
+            for(SinhVien sv : Database){
+                if (sv.getName().c_str() == name){
+                    infoFound = 1;
+                    hienThiDanhSach(sv, index);
+                }
+                index ++;
+            }
+            if (infoFound == 0)   cout << "\n    Name not found!\n";
+
             phim = 0;
             break;
 
         case 5:
-            sortByGPA();
-            hienThiDanhSach();
-            phim = 0;
+            sort(GPA);
+            phim = 7;
             break;
         
         case 6:
-            sortByName();
-            hienThiDanhSach();
-            phim = 0;
+            sort(NAME);
+            phim = 7;
             break;
 
         case 7:
-            hienThiDanhSach();
+            index = 1;
+
+            PRINT_TITLE();
+            for(SinhVien sv: Database){
+                hienThiDanhSach(sv, index);
+                index ++;
+            }
             phim = 0;
             break;
     
@@ -384,154 +436,67 @@ Menu::Menu(){
 }
 
 /*
- * Function: Menu::themSinhVien()
- * Description: This method is used for adding student information into an element and store in database
+ * Function: Menu::enterInfo()
+ * Description: This method is used for adding or updating student information into an element and store in database
  * Input:
  *    none
  * Output:
  *    none
 */
-void Menu::themSinhVien(){
+void Menu::enterInfo(uint8_t mode, uint8_t index){
     printf("\nNhap thong tin sinh vien\n");
     string name;
-    string s_gender;
-    TypeGender gender;
-    uint8_t age;
+    string gender;
+    int age;
     float diemToan;
     float diemLy;
     float diemHoa;
 
-    cout << "   Ten: ";
-    cin.ignore(100, '\n');
-    getline(cin, name);
+    INPUT_TYPE("   Ten: ", name);
 
     do
     {
-        cout << "   Gioi tinh(Nam/Nu): ";
-        cin >> s_gender;
-        if(s_gender == "Nam")   gender = NAM;
-        else if(s_gender == "Nu")   gender = NU;
-        else   printf("    FORMAT ERROR!!!\n\n");
-    } while (!(s_gender == "Nam" | s_gender == "Nu"));
+        INPUT_TYPE("   Gioi tinh(Nam/Nu): ", gender);
 
-    printf("   Tuoi: ");
-    scanf("%d", &age);
+        if (!(gender == "Nam" | gender == "Nu"))   printf("    FORMAT ERROR!!!\n\n");
 
+    } while (!(gender == "Nam" | gender == "Nu"));
+
+    INPUT_TYPE("   Tuoi: ", age);
+    
     do
     {
-        printf("   Diem Toan: ");
-        scanf("%f", &diemToan);
-        if (diemToan > 10 | diemToan < 0){
-            printf("    FORMAT ERROR!!!\n\n");
-        }
+        INPUT_TYPE("   Diem Toan: ", diemToan);
+
+        if (diemToan > 10 | diemToan < 0)   printf("    FORMAT ERROR!!!\n\n");
+        
     } while (diemToan > 10 | diemToan < 0);
     
     do
     {
-        printf("   Diem Ly: ");
-        scanf("%f", &diemLy);
-        if (diemLy > 10 | diemLy < 0){
-            printf("    FORMAT ERROR!!!\n\n");
-        }
+        INPUT_TYPE("   Diem Ly: ", diemLy);
+
+        if (diemLy > 10 | diemLy < 0)       printf("    FORMAT ERROR!!!\n\n");
+
     } while (diemLy > 10 | diemLy < 0);
 
     do
     {
-        printf("   Diem Hoa: ");
-        scanf("%f", &diemHoa);
-        if (diemHoa > 10 | diemHoa < 0){
-            printf("    FORMAT ERROR!!!\n\n");
-        }
+        INPUT_TYPE("   Diem Hoa: ", diemHoa);
+        if (diemHoa > 10 | diemHoa < 0)     printf("    FORMAT ERROR!!!\n\n");
+        
     } while (diemHoa > 10 | diemHoa < 0);
 
-    cout << "\nThem thong tin thanh cong!\n";
-
     SinhVien sv(name, gender, age, diemToan, diemLy, diemHoa);
-    Database.push_back(sv);
-}
 
-/*
- * Function: Menu::capNhatThongTin()
- * Description: This method is used for updating student information storing already in database
- * Input:
- *    none
- * Output:
- *    none
-*/
-void Menu::capNhatThongTin(){
-    uint8_t index = 0;
-    uint8_t id;
-    uint8_t idFound = 0;
-    string name;
-    string s_gender;
-    TypeGender gender;
-    uint8_t age;
-    float diemToan;
-    float diemLy;
-    float diemHoa;
-
-    cout << "\nNhap ID can cap nhat: ";
-    scanf("%d", &id);
-
-    for(SinhVien sv : Database){
-        idFound = 1;
-        if(sv.getID() == id){
-            cout << "   Ten: ";
-            cin.ignore(100, '\n');
-            getline(cin, name);
-
-            do
-            {
-                cout << "   Gioi tinh(Nam/Nu): ";
-                cin >> s_gender;
-                if(s_gender == "Nam")   gender = NAM;
-                else if(s_gender == "Nu")   gender = NU;
-                else   printf("    FORMAT ERROR!!!\n\n");
-            } while (!(s_gender == "Nam" | s_gender == "Nu"));
-
-            printf("   Tuoi: ");
-            scanf("%d", &age);
-
-            do
-            {
-                cout << "   Diem Toan: ";
-                cin >> diemToan;
-                if (diemToan > 10 || diemToan < 0){
-                    printf("    FORMAT ERROR!!!\n\n");
-                }
-            } while (diemToan > 10 || diemToan < 0);
-    
-            do
-            {
-                cout << "   Diem Ly: ";
-                cin >> diemLy;
-                if (diemLy > 10 || diemLy < 0){
-                    printf("    FORMAT ERROR!!!\n\n");
-                }
-            } while (diemLy > 10 || diemLy < 0);
-
-            do
-            {
-                cout << "   Diem Hoa: ";
-                cin >> diemHoa;
-                if (diemHoa > 10 || diemHoa < 0){
-                    printf("    FORMAT ERROR!!!\n\n");
-                }
-            } while (diemHoa > 10 || diemHoa < 0);
-
-            sv.setName(name);
-            sv.setGender(gender);
-            sv.setAge(age);
-            sv.setDiemToan(diemToan);
-            sv.setDiemLy(diemLy);
-            sv.setDiemHoa(diemHoa);
-            Database[index] = sv;
-
-            cout << "\nCap nhat thong tin thanh cong!\n";
-        }
-        index++;
+    if (mode == 0){
+        Database.push_back(sv);
+        cout << "\nThem thong tin thanh cong!\n";
     }
-    if(idFound == 0) cout << "\n    ID not found!\n";
+    else if(mode == 1){
+        Database[index] = sv;
+        cout << "\nCap nhat thong tin thanh cong!\n";
+    }
 }
 
 /*
@@ -547,8 +512,7 @@ void Menu::xoaSinhVien(){
     uint8_t index = 0;
     uint8_t idFound = 0;
 
-    cout << "\nNhap ID cua sinh vien can xoa: ";
-    scanf("%d", &id);
+    INPUT_TYPE("\nNhap ID cua sinh vien can xoa: ", id);
 
     for(SinhVien sv : Database){
         if(sv.getID() == id){
@@ -562,100 +526,34 @@ void Menu::xoaSinhVien(){
 }
 
 /*
- * Function: Menu::timSinhVien()
- * Description: This method is used for searching student information in database
+ * Function: Menu::sort()
+ * Description: This method is used for sorting student list in database by average score or by name
  * Input:
  *    none
  * Output:
  *    none
 */
-void Menu::timSinhVien(){
-    string name;
-    uint8_t STT = 1;
-    uint8_t nameFound = 0;
-    string s_Gender;
-    string s_rankAcademic;
-
-    cout << "\nNhap ten sinh vien can tim: ";
-    cin.ignore(100, '\n');
-    getline(cin, name);
-
-    cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
-    cout << "| STT |  ID   |      TEN      | GIOI TINH | TUOI | DIEM TOAN | DIEM LY | DIEM HOA | DTB  |    HOC LUC      |\n";
-    cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
-
-    for(SinhVien sv : Database){
-        if (sv.getName().c_str() == name){
-            nameFound = 1;
-            if(sv.getGender() == NAM) s_Gender = "Nam";
-            else s_Gender = "Nu";
-
-            if(sv.getHocLuc() == GIOI) s_rankAcademic = "Gioi";
-            else if(sv.getHocLuc() == KHA) s_rankAcademic = "Kha";
-            else if(sv.getHocLuc() == TRUNG_BINH) s_rankAcademic = "Trung Binh";
-            else s_rankAcademic = "Yeu";
-
-            
-            printf("| %3d ", STT);
-            printf("|  %-5d", sv.getID());
-            printf("| %-14s", sv.getName().c_str());
-            printf("|    %-7s", s_Gender.c_str());
-            printf("|  %-4d", sv.getAge());
-            printf("|    %-7.2f", sv.getDiemToan());
-            printf("|   %-6.2f", sv.getDiemLy());
-            printf("|   %-7.2f", sv.getDiemHoa());
-            printf("| %-5.2f", sv.getDiemTrungBinh());
-            printf("|   %-14s|\n", s_rankAcademic.c_str());
-            cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
-        }
-        STT++;
+void Menu::sort(TypeOfSort sortType){
+    SinhVien temp;
+    for(uint8_t i=0; i < Database.size()-1; i++){
+        for (uint8_t j = i+1; j < Database.size(); j++){
+            if (sortType == GPA){
+                if(Database[i].getDiemTrungBinh() > Database[j].getDiemTrungBinh()){
+                    temp=Database[j];
+                    Database[j]=Database[i];
+                    Database[i]=temp;
+                }
+            }
+            else if (sortType == NAME){
+                if(Database[i].getName().compare(Database[j].getName()) > 0){
+                    temp=Database[j];
+                    Database[j]=Database[i];
+                    Database[i]=temp;
+                } 
+            }
+        }           
     }
-    if(nameFound == 0) cout << "\n    Khong tim thay ten trong danh sach!\n";
 }
-
-/*
- * Function: Menu::sortByGPA()
- * Description: This method is used for sorting student list in database by average score
- * Input:
- *    none
- * Output:
- *    none
-*/
-void Menu::sortByGPA(){
-    for(uint8_t i=0; i < Database.size()-1; i++){
-        for (uint8_t j = i+1; j < Database.size(); j++){
-            if(Database[i].getDiemTrungBinh() > Database[j].getDiemTrungBinh()){
-                Database.push_back(Database[i]);
-                Database[Database.size()-1]=Database[j];
-                Database[j]=Database[i];
-                Database[i]=Database[Database.size()-1];
-                Database.pop_back();
-            }
-        }
-    }           
-}
-
-/*
- * Function: Menu::sortByName()
- * Description: This method is used for sorting student list in database by name
- * Input:
- *    none
- * Output:
- *    none
-*/
-void Menu::sortByName(){
-    for(uint8_t i=0; i < Database.size()-1; i++){
-        for (uint8_t j = i+1; j < Database.size(); j++){
-            if(Database[i].getName().compare(Database[j].getName()) > 0){
-                Database.push_back(Database[i]);
-                Database[Database.size()-1]=Database[j];
-                Database[j]=Database[i];
-                Database[i]=Database[Database.size()-1];
-                Database.pop_back();
-            }
-        }
-    }           
-}   
 
 /*
  * Function: Menu::hienThiDanhSach()
@@ -665,40 +563,16 @@ void Menu::sortByName(){
  * Output:
  *    none
 */
-void Menu::hienThiDanhSach(){
-    int STT=1;
-    string s_Gender;
+void Menu::hienThiDanhSach(SinhVien sv, uint8_t index){
     string s_rankAcademic;
 
-    cout << "\nDANH SACH SINH VIEN\n";
-    cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
-    cout << "| STT |  ID   |      TEN      | GIOI TINH | TUOI | DIEM TOAN | DIEM LY | DIEM HOA | DTB  |    HOC LUC      |\n";
-    cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
+    if(sv.getHocLuc() == GIOI) s_rankAcademic = "Gioi";
+    else if(sv.getHocLuc() == KHA) s_rankAcademic = "Kha";
+    else if(sv.getHocLuc() == TRUNG_BINH) s_rankAcademic = "Trung Binh";
+    else s_rankAcademic = "Yeu";
 
-    for (SinhVien sv : Database){
-
-        if(sv.getGender() == NAM) s_Gender = "Nam";
-        else s_Gender = "Nu";
-
-        if(sv.getHocLuc() == GIOI) s_rankAcademic = "Gioi";
-        else if(sv.getHocLuc() == KHA) s_rankAcademic = "Kha";
-        else if(sv.getHocLuc() == TRUNG_BINH) s_rankAcademic = "Trung Binh";
-        else s_rankAcademic = "Yeu";
-
-        printf("| %3d ", STT);
-        printf("|  %-5d", sv.getID());
-        printf("| %-14s", sv.getName().c_str());
-        printf("|    %-7s", s_Gender.c_str());
-        printf("|  %-4d", sv.getAge());
-        printf("|    %-7.2f", sv.getDiemToan());
-        printf("|   %-6.2f", sv.getDiemLy());
-        printf("|   %-7.2f", sv.getDiemHoa());
-        printf("| %-5.2f", sv.getDiemTrungBinh());
-        printf("|   %-14s|\n", s_rankAcademic.c_str());
-        cout << "+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n";
-
-        STT++; 
-    }
+    printf("| %-4d|  %-5d| %-14s|    %-7s|  %-4d|    %-7.2f|   %-6.2f|   %-7.2f| %-5.2f|   %-14s|\n", index, sv.getID(), sv.getName().c_str(), sv.getGender().c_str(), sv.getAge(), sv.getDiemToan(),sv.getDiemLy(), sv.getDiemHoa(), sv.getDiemTrungBinh(), s_rankAcademic.c_str());
+    printf("+-------------+---------------+-----------+------+-----------+---------+----------+------+-----------------+\n");
 }
 
 int main(int argc, char const *argv[]){
